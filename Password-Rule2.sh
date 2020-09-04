06fc98dbde048b4745a35246cf3a1ac0bfb83fd2
unknown <shravyak094@gmail.com>
2020-09-04 17:40:39 +0530

    Added: Password-Rule2.sh file

--- Password-Rule2.sh
+++ Password-Rule2.sh
@@ -7,7 +7,8 @@ read -p "Enter password: " password
 nameRegex="^[[:upper:]][[:lower:]]{3,}$"
 emailRegex="^([a-z0-9]+[-._+]{0,1}[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$"
 mobileRegex="^[9][1][6-9][0-9]{9}$"
-passwordRegex="[A-Z]+[a-ZA-Z0-9.!@#$&_-]{7,}"
+minPasswordLength=8
+uppercaseRegex="[[:upper:]]+"
 if [[ $firstName =~ $nameRegex ]]
 then
   echo "First name is valid"
@@ -32,7 +33,7 @@ then
 else
 	echo "Mobile number is not valid"
 fi
-if [[ $password =~ $passwordRegex ]]
+if [[ ${#password} -ge $minPasswordLength && $password =~ $uppercaseRegex ]]
 then
 	echo "Password is valid"
 else
